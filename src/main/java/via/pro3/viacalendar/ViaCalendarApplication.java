b02b51d12ae52c6708c123cba745511a86e2c467
tomasz-grzeskowiak <tom.grzeskow@gmail.com>
2025-10-22 11:42:29 +0200

    fix

---
+++ src/main/java/via/pro3/viacalendar/ViaCalendarApplication.java
@@ -0,0 +1,13 @@
+package via.pro3.viacalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/via/pro3/viacalendar/ViaCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package via.pro3.viacalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
